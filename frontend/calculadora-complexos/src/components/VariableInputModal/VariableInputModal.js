64bc36246ba50782d008f1b036b2feb71c305e2b
Camilo2725 <camilofondado2705@gmail.com>
2025-12-01 16:18:38 -0300

    Limitar caracteres permitidos no prompt de variável

--- frontend/calculadora-complexos/src/components/VariableInputModal/VariableInputModal.js
+++ frontend/calculadora-complexos/src/components/VariableInputModal/VariableInputModal.js
@@ -50,9 +50,15 @@ export default function VariableInputModal({ onClose, onConfirm, tokens, clearVa
                             <label style={{ display: 'block', marginBottom: 6, marginRight: 10 }}>{`${variavel.toUpperCase()}=`}</label>
                             <input
                                 placeholder={`${variavel}`}
-                                type='number'
+                                type='tel'
+                                pattern="[0-9\-]*"
                                 value={valores[variavel] || ""}
-                                onChange={(e) => updateValue(variavel, e.target.value)}
+                                onChange={(e) => {
+                                    const val = e.target.value;
+                                    if (/^[-0-9]*$/.test(val)) {
+                                        setValores(prev => ({ ...prev, [variavel]: val }));
+                                    }
+                                }}
                             />
                         </div>
                     ))}
